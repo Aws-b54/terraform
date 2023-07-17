530a930407c0bdddc1664db13451be324f93003a
manoj kumar <chaitra.990230@gmail.com>
2023-07-17 19:02:34 +0530

    auto push

--- vars/02-vars.tf
+++ vars/02-vars.tf
@@ -9,3 +9,14 @@ output "sample-op" {
 output "sample-var-output" {
  value ="value of the varaiable is${var.sample}"
 }
+
+varaiable "example_list" {
+default = ["AWS",
+          "DevOps",
+          54,
+          "terraform",
+          ]
+}
+output "example_list_op" {
+    value = Welcome to ${var_example_list[1]} and ${var_example_list[0]} training batch ${var_list_example[2]} and topic is ${var_example_list[3]}
+}
