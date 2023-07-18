68a212fedce5ce8b2dc45e410d54b3de299ca0bf
manoj kumar <chaitra.990230@gmail.com>
2023-07-18 11:55:37 +0530

    auto push

---
+++ 03-vars.tf/sample.tf
@@ -0,0 +1,11 @@
+variable "city" {}
+
+output "city_name" {
+    value = "our city name is ${var.city}"
+}
+
+#varaiable "state"{}
+
+#output "state_name" {
+    #value = our state name is ${var.state}"
+#}
